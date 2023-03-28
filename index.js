eb660e83d39124d8fe45fd4647e98daaad5bb31c
Abdurrahman Abuzaid <abdurrahman.abzd@gmail.com>
2023-03-28 14:48:52 -0400

    solved assignment

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+let num1 = 31
+let num2 = 2
+let num3 = 31
+let num4 = 2
 
-// Write your code here
+
+let multiply = num1*num2;
+let random = Math.floor(Math.random() * 10);
+let mod = 4
+let max = 20
